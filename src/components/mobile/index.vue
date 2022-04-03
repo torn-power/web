2c91af4c6bce1b15a16559dfa8b43333dade1947
caohuiboss <2550465923@qq.com>
2022-04-03 16:23:04 +0800

    分离项目

---
+++ src/components/mobile/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mobile-wrap">
+    <header>首页</header>
+
+    <div class="wrap"></div>
+  </div>
+</template>
+<script setup></script>
+<style lang="less" scoped>
+.mobile-wrap {
+  width: 100vw;
+  min-height: 200vh;
+  background-image: url("/img/mobile_bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  header {
+    height: 70px;
+    color: white;
+    font-size: 16px;
+  }
+}
+</style>
---
+++ src/components/pc/index.vue
@@ -0,0 +1,3 @@
+<template>
+pc
+</template>
