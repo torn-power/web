b15407ad37cb7b173a3acf1bbb41f15f63655020
zhaobowen <1964471757@qq.com>
2022-04-10 21:58:05 +0800

    完成数据共享的布局

--- src/components/pc/index.vue
+++ src/components/pc/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="pc-wrap">pc page</div>
+  <div class="pc-wrap">
+    <div class="header">导航</div>
+    <div class="content">
+      <div class="share-data-wrap">
+        <div class="title">共享数据</div>
+        <div class="share-data-content-wrap">
+          <div class="content-item-wrap">
+            <div class="num">1,545,632,123</div>
+            <div class="name">累计为用户提供<span class="sign-name">&nbsp;带宽</span></div>
+          </div>
+          <div class="content-item-wrap">
+            <div class="num">1,545,632 K</div>
+            <div class="name">累计为用户提供<span class="sign-name">&nbsp;能量</span></div>
+          </div>
+          <div class="content-item-wrap">
+            <div class="num">1,545,632,123</div>
+            <div class="name">累计为用户节省<span class="sign-name">&nbsp;TRX</span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup></script>
 
 <style lang="less" scoped>
 @import "index.less";
-.actived {
-  color: rgb(107, 107, 141);
-}
+
 </style>
