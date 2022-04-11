eba411f1842a3457605f78840897e8d003b42921
zhaobowen <1964471757@qq.com>
2022-04-11 18:30:09 +0800

    完成资源租赁

--- src/components/pc/index.vue
+++ src/components/pc/index.vue
@@ -2,6 +2,7 @@
   <div class="pc-wrap">
     <div class="header">导航</div>
     <div class="content">
+      <!-- 共享数据 -->
       <div class="share-data-wrap">
         <div class="title">共享数据</div>
         <div class="share-data-content-wrap">
@@ -16,6 +17,149 @@
           <div class="content-item-wrap">
             <div class="num">1,545,632,123</div>
             <div class="name">累计为用户节省<span class="sign-name">&nbsp;TRX</span></div>
+          </div>
+        </div>
+      </div>
+      <!-- 推荐奖励 -->
+      <div class="recommend-reward">
+        <div class="title">推荐奖励</div>
+        <div class="recommend-reward-wrap">
+          <div class="reward-detail-content-wrap">
+            <div class="reward-detail-item-wrap">
+              <img class="reward-icon" src="/img/icon-1.png" alt="" />
+              <div class="reward-detail-wrap">
+                <div class="reward-name">
+                    钱包余额
+                </div>
+                <div class="reward-num">
+                  0.00
+                </div>
+              </div>
+            </div>
+            <div class="reward-detail-item-wrap">
+              <img class="reward-icon" src="/img/icon-2.png" alt="" />
+              <div class="reward-detail-wrap">
+                <div class="reward-name">
+                    获得的奖励
+                </div>
+                <div class="reward-num">
+                  0.00
+                </div>
+              </div>
+            </div>
+            <div class="reward-detail-item-wrap">
+              <img class="reward-icon" src="/img/icon-3.png" alt="" />
+              <div class="reward-detail-wrap">
+                <div class="reward-name">
+                    已邀请人数
+                </div>
+                <div class="reward-num">
+                  0.00
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="recommend-url-wrap">
+            <div class="recommend-url-tips">复制您的推荐链接，邀请并获得海量奖励。</div>
+            <div class="recommend-url-content-wrap">
+              <div class="url-content-wrap">
+                https://www.figma.com/id=0%3A1
+              </div>
+              <div class="copy-url-line"></div>              
+              <img class="copy-img" src="/img/copy.png" alt="" />
+              <div class="copy-btn">复制到剪贴板</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 资源租赁 -->
+      <div class="resource-lease-wrap">
+        <div class="title">资源租赁</div>
+        <div class="resource-lease-content-wrap">
+          <div class="resource-lease-content-form">
+            <a-form name="formState">
+                <a-form-item>
+                  <a-row>
+                    <a-col :span="5">
+                      <div class="color-1 font-24 form-item-name">资源接收地址</div>
+                    </a-col>
+                    <a-col :span="19">
+                      <a-input placeholder="资源接收地址"></a-input>
+                    </a-col>
+                  </a-row>
+                </a-form-item>
+                <a-form-item>
+                  <a-row>
+                    <a-col :span="5">
+                      <div class="color-1 font-24 form-item-name">资源类型</div>
+                    </a-col>
+                    <a-col :span="19">
+                      <div class="form-item-value">
+                        <a-radio-group>
+                          <a-radio value="ENERGY" class="font-24" style="color: white">{{
+                            $t("global.energy")
+                          }}</a-radio>
+                          <a-radio
+                            value="BANDWIDTH"
+                            class="font-24"
+                            style="color: white"
+                            >{{ $t("global.bandwidth") }}</a-radio
+                          >
+                        </a-radio-group>
+                      </div>
+                    </a-col>
+                  </a-row>
+                </a-form-item>
+                <a-form-item>
+                  <a-row>
+                    <a-col :span="5">
+                      <div class="color-1 font-24 form-item-name">资源数量</div>
+                    </a-col>
+                    <a-col :span="19">
+                      <a-input-number
+                        v-if="1"
+                        :placeholder="$t('tip.pledgeEnergy', { amount: 300 })"
+                        :precision="0"
+                      />
+                      <a-input-number
+                        v-else
+                        :placeholder="$t('tip.pledegBandWidth', { amount: 500 })"
+                        :precision="0"
+                      />
+                    </a-col>
+                  </a-row>
+                </a-form-item>
+                <a-form-item>
+                  <a-row>
+                    <a-col :span="5">
+                      <div class="color-1 font-24 form-item-name">单价(sun) / 天</div>
+                    </a-col>
+                    <a-col :span="19">
+                      <a-input-number
+                        :min="0"
+                        class="input-search"
+                        placeholder="单价"
+                      ></a-input-number>
+                    </a-col>
+                  </a-row>
+                </a-form-item>
+                <div>
+                  <a-row>
+                    <a-col :span="5">
+                      <div class="color-1 font-24 form-item-name">订单金额</div>
+                    </a-col>
+                    <a-col :span="19">
+                      <div class="form-item-value">
+                        <span style="font-style: normal;font-weight: 600;font-size: 24px;line-height: 24px;color: #FFCC17;margin-right:4px;">196.00</span>
+                        <span style="font-style: normal;font-weight: 400;font-size: 14px;line-height: 22px;color: #FFCC17;margin-right:12px;">TRX</span>
+                        <span style="font-style: normal;font-weight: 400;font-size: 14px;line-height: 22px;color: #FFCC17;margin-right:4px;">相比燃烧TRX获得资源节省</span>
+                        <span style="font-style: normal;font-weight: 600;font-size: 18px;line-height: 24px;color: #FFFFFF;margin-right:4px;">196.00</span>
+                        <span style="font-style: normal;font-weight: 400;font-size: 14px;line-height: 22px;color: #FFFFFF;">TRX</span>
+                      </div>
+                    </a-col>
+                  </a-row>
+                </div>
+            </a-form>
           </div>
         </div>
       </div>
