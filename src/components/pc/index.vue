3e4f19ed54481413aa8896fd9cc2590389d87365
caohuiboss <2550465923@qq.com>
2022-04-21 22:29:24 +0800

    添加逻辑

--- src/components/pc/index.vue
+++ src/components/pc/index.vue
@@ -6,88 +6,115 @@
         <span class="logo-name">TRON</span>
       </div>
       <div class="left">
-        <span class="wallte">{{$t("global.myWallet")}}</span>
-        <div class="header-line"></div>
-        <div class="language-wrap">
-          <span class="language-item">EN</span>
-          <span>/</span>
-          <span class="language-item">繁体</span>
-          <span>/</span>
-          <span class="language-item active">简体</span>
-        </div>
+        <a-space>
+          <span class="wallte">
+            <a-button
+              v-if="!ownerAddress"
+              type="primary"
+              shape="round"
+              @click="linkWallet"
+              >{{ $t("global.myWallet") }}</a-button
+            >
+            <div v-if="ownerAddress">
+              {{ uzipAddress(ownerAddress) }}
+            </div>
+          </span>
+          <div class="header-line"></div>
+          <div class="language-wrap">
+            <a-space>
+              <div
+                :class="lang === 'zh' && 'actived'"
+                @click="changeLang('zh')"
+              >
+                中文
+              </div>
+              /
+              <div
+                :class="lang === 'en' && 'actived'"
+                @click="changeLang('en')"
+              >
+                En
+              </div>
+            </a-space>
+          </div>
+        </a-space>
       </div>
     </div>
     <div class="content">
       <!-- 共享数据 -->
       <div class="share-data-wrap">
-        <div class="title">{{$t("global.shareData")}}</div>
+        <div class="title">{{ $t("global.shareData") }}</div>
         <div class="share-data-content-wrap">
           <div class="content-item-wrap">
-            <div class="num">{{ toLocaleString(rent.totalBandWidth || 0) }}</div>
+            <div class="num">
+              {{ toLocaleString(rent.totalBandWidth || 0) }}
+            </div>
             <div class="name">
               <!-- 累计为用户提供<span class="sign-name">&nbsp;带宽</span> -->
-              {{$t("global.providedUsersBandWidth")}}
+              {{ $t("global.providedUsersBandWidth") }}
             </div>
           </div>
           <div class="content-item-wrap">
             <div class="num">{{ toLocaleString(rent.totalEnergy || 0) }} K</div>
             <div class="name">
               <!-- 累计为用户提供<span class="sign-name">&nbsp;能量</span> -->
-              {{$t("global.providedUsersEnergy")}}
+              {{ $t("global.providedUsersEnergy") }}
             </div>
           </div>
           <div class="content-item-wrap">
             <div class="num">{{ toLocaleString(rent.totalTrx / 1000000) }}</div>
             <div class="name">
               <!-- 累计为用户节省<span class="sign-name">&nbsp;TRX</span> -->
-              {{$t("global.saveUserTrx")}}
+              {{ $t("global.saveUserTrx") }}
             </div>
           </div>
         </div>
       </div>
       <!-- 推荐奖励 -->
       <div class="recommend-reward">
-        <div class="title">{{$t("global.recommendReward")}}</div>
+        <div class="title">{{ $t("global.recommendReward") }}</div>
         <div class="recommend-reward-wrap">
           <div class="reward-detail-content-wrap">
             <div class="reward-detail-item-wrap">
               <img class="reward-icon" src="/img/icon-1.png" alt="" />
               <div class="reward-detail-wrap">
-                <div class="reward-name">{{$t("global.walletBalance")}}</div>
-                <div class="reward-num">{{ accountResouce.balance || 0 }} TRX</div>
+                <div class="reward-name">{{ $t("global.walletBalance") }}</div>
+                <div class="reward-num">
+                  {{ accountResouce.balance || 0 }} TRX
+                </div>
               </div>
             </div>
             <div class="reward-detail-item-wrap">
               <img class="reward-icon" src="/img/icon-2.png" alt="" />
               <div class="reward-detail-wrap">
-                <div class="reward-name">{{$t("global.gainedReward")}}</div>
+                <div class="reward-name">{{ $t("global.gainedReward") }}</div>
                 <div class="reward-num">0.00</div>
               </div>
             </div>
             <div class="reward-detail-item-wrap">
               <img class="reward-icon" src="/img/icon-3.png" alt="" />
               <div class="reward-detail-wrap">
-                <div class="reward-name">{{$t("global.invitedNumbers")}}</div>
+                <div class="reward-name">{{ $t("global.invitedNumbers") }}</div>
                 <div class="reward-num">0.00</div>
               </div>
             </div>
           </div>
           <div class="recommend-url-wrap">
             <div class="recommend-url-tips">
-              {{$t("global.copyLinkTips")}}
+              {{ $t("global.copyLinkTips") }}
             </div>
-            <div class="recommend-url-content-wrap">
+            <div class="recommend-url-content-wrap" @click="copyText">
               <div class="url-content-wrap">https://www.figma.com/id=0%3A1</div>
               <div class="copy-url-line"></div>
               <img class="copy-img" src="/img/copy.png" alt="" />
-              <div class="copy-btn">{{$t("global.copyToClipboard")}}</div>
+              <div class="copy-btn">{{ $t("global.copyToClipboard") }}</div>
             </div>
           </div>
         </div>
       </div>
       <!-- 资源租赁 -->
       <div class="resource-lease-wrap">
-        <div class="title">{{$t("global.resourceType")}}</div>
+        <div class="title">{{ $t("global.resourceType") }}</div>
         <div class="resource-lease-content-wrap">
           <div class="resource-lease-content-form">
             <a-form name="formState" :model="formState">
@@ -95,7 +122,7 @@
                 <a-row>
                   <a-col :span="5">
                     <div class="color-1 font-24 form-item-name">
-                      {{$t("global.resourceReceAddress")}}
+                      {{ $t("global.resourceReceAddress") }}
                     </div>
                   </a-col>
                   <a-col :span="19">
@@ -109,7 +136,9 @@
               <a-form-item v-bind="validateInfos.resource">
                 <a-row>
                   <a-col :span="5">
-                    <div class="color-1 font-24 form-item-name">{{$t("global.resourceType")}}</div>
+                    <div class="color-1 font-24 form-item-name">
+                      {{ $t("global.resourceType") }}
+                    </div>
                   </a-col>
                   <a-col :span="19">
                     <div class="form-item-value">
@@ -134,13 +163,17 @@
               <a-form-item v-bind="validateInfos.amount">
                 <a-row>
                   <a-col :span="5">
-                    <div class="color-1 font-24 form-item-name">{{$t("global.numResource")}}</div>
+                    <div class="color-1 font-24 form-item-name">
+                      {{ $t("global.numResource") }}
+                    </div>
                   </a-col>
                   <a-col :span="19">
                     <a-input-number
                       v-if="formState.resource === 'ENERGY'"
                       :placeholder="
-                        $t('tip.pledgeEnergy', { amount: config.minEnergyNumber })
+                        $t('tip.pledgeEnergy', {
+                          amount: config.minEnergyNumber,
+                        })
                       "
                       :precision="0"
                       v-model:value="formState.amount"
@@ -164,7 +197,7 @@
                 <a-row>
                   <a-col :span="5">
                     <div class="color-1 font-24 form-item-name">
-                      {{$t('tip.unitPricDay')}}
+                      {{ $t("tip.unitPricDay") }}
                     </div>
                   </a-col>
                   <a-col :span="19">
@@ -186,7 +219,9 @@
               <div>
                 <a-row>
                   <a-col :span="5">
-                    <div class="color-1 font-24 form-item-name">{{$t('tip.orderAmount')}}</div>
+                    <div class="color-1 font-24 form-item-name">
+                      {{ $t("tip.orderAmount") }}
+                    </div>
                   </a-col>
                   <a-col :span="19">
                     <div class="form-item-value">
@@ -199,7 +234,7 @@
                           color: #ffcc17;
                           margin-right: 4px;
                         "
-                        >{{needTrxCount || 0}}</span
+                        >{{ needTrxCount || 0 }}</span
                       >
                       <span
                         style="
@@ -221,7 +256,7 @@
                           color: #ffcc17;
                           margin-right: 4px;
                         "
-                        >{{$t('tip.burnTrxTips')}}</span
+                        >{{ $t("tip.burnTrxTips") }}</span
                       >
                       <span
                         style="
@@ -257,13 +292,13 @@
             size="large"
             @click="submitFreeze"
             :disabled="!ownerAddress"
-            >{{$t('tip.placeOrder')}}</a-button
+            >{{ $t("tip.placeOrder") }}</a-button
           >
         </div>
       </div>
       <!-- 当前订单 -->
       <div class="current-order-wrap">
-        <div class="title">{{$t('tip.currentOrder')}}</div>
+        <div class="title">{{ $t("tip.currentOrder") }}</div>
         <div class="current-order-content-wrap">
           <div style="width: 100%">
             <a-select
@@ -283,28 +318,42 @@
             <div class="order-table-header-wrap">
               <a-row>
                 <a-col :span="9">
-                  <div class="order-table-header-item">{{$t('tip.orderList')}}</div>
+                  <div class="order-table-header-item">
+                    {{ $t("tip.orderList") }}
+                  </div>
                 </a-col>
                 <a-col :span="9">
-                  <div class="order-table-header-item">{{$t('tip.sellerIncome')}}</div>
+                  <div class="order-table-header-item">
+                    {{ $t("tip.sellerIncome") }}
+                  </div>
                 </a-col>
                 <a-col :span="6">
-                  <div class="order-table-header-item">{{$t('tip.operation')}}</div>
+                  <div class="order-table-header-item">
+                    {{ $t("tip.operation") }}
+                  </div>
                 </a-col>
               </a-row>
             </div>
             <div class="order-table-body-wrap">
-              <div class="order-table-body-row" v-for="(record, i) in tableData" :key="i">
+              <div
+                class="order-table-body-row"
+                v-for="(record, i) in tableData"
+                :key="i"
+              >
                 <a-row>
                   <a-col :span="9">
                     <div class="order-table-body-item">
                       <div class="body-item-line">
-                        <span class="line-name">{{$t('tip.priceOfDay')}}：</span>
+                        <span class="line-name"
+                          >{{ $t("tip.priceOfDay") }}：</span
+                        >
                         <span class="line-num">{{ record.unitPrice }}</span>
                         <span class="line-unit">sun</span>
                       </div>
                       <div class="body-item-line">
-                        <span class="line-name">{{$t('tip.bandwidth')}}：</span>
+                        <span class="line-name"
+                          >{{ $t("tip.bandwidth") }}：</span
+                        >
                         <span class="line-num">{{ record.resourceValue }}</span>
                       </div>
                     </div>
@@ -312,13 +361,17 @@
                   <a-col :span="9">
                     <div class="order-table-body-item">
                       <div class="body-item-line">
-                        <span class="line-name">{{$t('tip.income')}}：</span>
-                        <span class="line-num">{{ parseInt(record.aCommission / 1000000) }}</span>
+                        <span class="line-name">{{ $t("tip.income") }}：</span>
+                        <span class="line-num">{{
+                          parseInt(record.aCommission / 1000000)
+                        }}</span>
                         <span class="line-unit">TRX</span>
                       </div>
                       <div class="body-item-line">
-                        <span class="line-name">{{$t('tip.freeze')}}：</span>
-                        <span class="line-num">{{ record.frozenBalance / 1000000 }}TRX</span>
+                        <span class="line-name">{{ $t("tip.freeze") }}：</span>
+                        <span class="line-num"
+                          >{{ record.frozenBalance / 1000000 }}TRX</span
+                        >
                       </div>
                     </div>
                   </a-col>
@@ -332,13 +385,15 @@
                         :disabled="!ownerAddress"
                         style="margin: 0 auto"
                         @click="submitSoldForm(record)"
-                        >{{$t('tip.sell')}}</a-button
+                        >{{ $t("tip.sell") }}</a-button
                       >
                     </div>
                   </a-col>
                 </a-row>
               </div>
-              <div class="no-data" v-if="tableData.length === 0">{{$t('tip.noDate')}}</div>
+              <div class="no-data" v-if="tableData.length === 0">
+                {{ $t("tip.noDate") }}
+              </div>
             </div>
           </div>
         </div>
@@ -356,4 +411,7 @@ export default defineComponent({
 
 <style lang="less" scoped>
 @import "index.less";
+.actived {
+  color: rgb(197, 237, 19);
+}
 </style>
