ae6d3cccaf4f64ae9fd83342fe9c7e420b0b6ac2
caohuiboss <2550465923@qq.com>
2022-06-04 13:04:03 +0800

    2

--- src/api/http.js
+++ src/api/http.js
@@ -1,41 +1,27 @@
 import service from "../utils/request";
 
-const URL = process.env.VUE_APP_TRON_API_URL;
-
-/**
- * 获取钱包地址信息
- * @param {*} address
- * @returns
- */
+// 获取钱包地址信息
 export const getAccountv2 = (address) => {
   return service({
-    url: `${URL}/accountv2`,
+    url: "/tron/accountv2",
     params: {
       address,
     },
   });
 };
 
-/**
- * 获取钱包资源
- * @param {*} params
- * @returns
- */
+// 获取钱包资源
 export const getAccountResource = (params) => {
   return service({
-    url: `${URL}/account/resource`,
+    url: `/tron/getAccountResource`,
     params,
   });
 };
 
-/**
- * 查询地址是否激活
- * @param {*} params
- * @returns
- */
+// 查询地址是否激活
 export const searchAddress = (params) => {
   return service({
-    url: `${URL}/search-main`,
+    url: `/tron/searchAddress`,
     params,
   });
 };
