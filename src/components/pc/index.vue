84db4abda87711e92b98764b7758bffdfcf58cf7
caohuiboss <2550465923@qq.com>
2022-07-12 23:11:04 +0800

    1

--- src/components/pc/index.vue
+++ src/components/pc/index.vue
@@ -36,12 +36,12 @@
         >
           {{ $t("global.announcementCenter") }}
         </div>
-        <!-- <div
+        <div
           @click="changeStatus('drop-activity')"
           :class="status === 'drop-activity' && 'actived'"
         >
           {{ $t("global.dropActivity") }}
-        </div> -->
+        </div>
         <div
           @click="changeStatus('helpCenter')"
           :class="status === 'helpCenter' && 'actived'"
